89a93c5a3fe582ef9a3a99eb593d72e844743b7a
cuoigl <cuoi.dev@gmail.com>
2023-03-16 14:54:03 +0700

    style init

--- apps/dinoshop/src/app/shared/footer/footer.component.ts
+++ apps/dinoshop/src/app/shared/footer/footer.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dino-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {}
