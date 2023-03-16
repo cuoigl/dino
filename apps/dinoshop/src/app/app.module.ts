476f65eed0e8b5367b81e5886600867abc255df6
cuoigl <cuoi.dev@gmail.com>
2023-03-16 22:39:58 +0700

    Prime Style

--- apps/dinoshop/src/app/app.module.ts
+++ apps/dinoshop/src/app/app.module.ts
@@ -1,6 +1,7 @@
-import { UiModule } from '@dino/ui';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -9,6 +10,10 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+
+import { UiModule } from '@dino/ui';
+
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
   declarations: [
@@ -20,8 +25,10 @@ import { FooterComponent } from './shared/footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     UiModule,
+    AccordionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- libs/ui/src/lib/banner/banner.component.ts
+++ libs/ui/src/lib/banner/banner.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dino-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent {}
