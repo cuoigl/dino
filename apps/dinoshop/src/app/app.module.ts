2dd4eba9a6f6442743cbadb99901cbf4838e512a
cuoigl <cuoi.dev@gmail.com>
2023-03-16 11:59:59 +0700

    creating routes dinoshop

--- apps/dinoshop/src/app/app.module.ts
+++ apps/dinoshop/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
--- apps/dinoshop/src/app/app.routes.ts
+++ apps/dinoshop/src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  },
+];
