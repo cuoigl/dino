380f8cc3886ad21e35d12e00a636e936b173f539
cuoigl <cuoi.dev@gmail.com>
2023-03-16 12:02:47 +0700

    create Header & Footer Components

--- apps/dinoshop/src/app/app.module.ts
+++ apps/dinoshop/src/app/app.module.ts
@@ -6,9 +6,17 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
---
+++ apps/dinoshop/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dino-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
