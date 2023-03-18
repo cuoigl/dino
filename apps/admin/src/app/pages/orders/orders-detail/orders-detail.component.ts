4c643c2d5c842439a1d3e65fcc0f5ec7a00c63da
cuoigl <cuoi.dev@gmail.com>
2023-03-18 20:28:34 +0700

    Order Details Component

--- apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
+++ apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ActivatedRoute } from '@angular/router';
+
+import { Order, OrdersService } from '@dino/orders';
 
 @Component({
   selector: 'admin-orders-detail',
   templateUrl: './orders-detail.component.html',
   styles: [],
 })
-export class OrdersDetailComponent {}
+export class OrdersDetailComponent implements OnInit {
+  order: Order;
+  constructor(
+    private ordersService: OrdersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this._getOrder();
+  }
+
+  private _getOrder() {
+    this.route.params.subscribe((params) => {
+      if (params.id) {
+        this.ordersService.getOrder(params.id).subscribe((order) => {
+          this.order = order;
+        });
+      }
+    });
+  }
+}
--- apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,5 +1,29 @@
+import { Router } from '@angular/router';
 import { Order, OrdersService } from '@dino/orders';
 import { Component, OnInit } from '@angular/core';
+
+const ORDER_STATUS = {
+  0: {
+    label: 'Pending',
+    color: 'primary',
+  },
+  1: {
+    label: 'Processed',
+    color: 'warning',
+  },
+  2: {
+    label: 'Shipped',
+    color: 'warning',
+  },
+  3: {
+    label: 'Delivered',
+    color: 'success',
+  },
+  4: {
+    label: 'Failed',
+    color: 'danger',
+  },
+};
 
 @Component({
   selector: 'admin-orders-list',
@@ -8,12 +32,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersListComponent implements OnInit {
   orders: Order[] = [];
+  orderStatus = ORDER_STATUS;
 
-  constructor(private ordersService: OrdersService) {}
+  constructor(private ordersService: OrdersService, private router: Router) {}
 
   ngOnInit() {
     this._getOrders();
+    console.log(this.orderStatus);
   }
+
+  showOrder(orderId) {
+    this.router.navigateByUrl(`/orders/${orderId}`);
+  }
+
+  deleteOrder(orderId) {}
 
   _getOrders() {
     this.ordersService.getOrders().subscribe((orders) => {
