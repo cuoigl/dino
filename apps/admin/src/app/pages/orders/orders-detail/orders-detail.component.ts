76df14a3ebcec573bf88b2c9617f66f179d92beb
cuoigl <cuoi.dev@gmail.com>
2023-03-18 20:58:24 +0700

    Order Address and Customer Info

--- apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
+++ apps/admin/src/app/pages/orders/orders-detail/orders-detail.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Order, OrdersService } from '@dino/orders';
+import { UsersService } from '@dino/users';
 
 @Component({
   selector: 'admin-orders-detail',
@@ -13,6 +14,7 @@ export class OrdersDetailComponent implements OnInit {
   order: Order;
   constructor(
     private ordersService: OrdersService,
+    private usersService: UsersService,
     private route: ActivatedRoute
   ) {}
 
@@ -28,5 +30,9 @@ export class OrdersDetailComponent implements OnInit {
         });
       }
     });
+  }
+
+  getCountryName(countryKey: string) {
+    if (countryKey) return this.usersService.getCountry(countryKey);
   }
 }
