945237278a99f7ec3e9f2af26b8755c8f4bcd6d0
cuoigl <cuoi.dev@gmail.com>
2023-03-19 18:43:40 +0700

    Categories Banner

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
+import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [ProductsSearchComponent],
-  exports: [ProductsSearchComponent],
+  imports: [CommonModule, HttpClientModule, RouterModule],
+  declarations: [ProductsSearchComponent, CategoriesBannerComponent],
+  exports: [ProductsSearchComponent, CategoriesBannerComponent],
 })
 export class ProductsModule {}
