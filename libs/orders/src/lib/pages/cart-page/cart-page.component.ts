c648ff76940ee4b48ff3978f8ccd102eb703afb1
cuoigl <cuoi.dev@gmail.com>
2023-03-20 19:43:52 +0700

    Connect the Cart with Products

--- libs/orders/src/lib/pages/cart-page/cart-page.component.ts
+++ libs/orders/src/lib/pages/cart-page/cart-page.component.ts
@@ -1,16 +1,31 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../../services/cart.service';
+import { ProductsService } from '@dino/products';
+
 @Component({
   selector: 'orders-cart-page',
   templateUrl: './cart-page.component.html',
   styles: [],
 })
 export class CartPageComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private cartService: CartService,
+    private productService: ProductsService
+  ) {}
 
   ngOnInit() {
-    console.log();
+    this._getCartDetails();
+  }
+
+  private _getCartDetails() {
+    this.cartService.cart$.pipe().subscribe((respCart) => {
+      respCart.items.forEach((cartItem) => {
+        console.log(cartItem);
+      });
+    });
   }
 
   backToShop() {
