fcac374963fbc7679d0ef677a70831b5603a22ac
cuoigl <cuoi.dev@gmail.com>
2023-03-20 23:12:11 +0700

    Thank you Page

--- libs/orders/src/lib/pages/checkout-page/checkout-page.component.ts
+++ libs/orders/src/lib/pages/checkout-page/checkout-page.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 import { UsersService } from '@dino/users';
+import { Cart } from '../../models/cart';
+
+import { Order } from '../../models/order';
 import { OrderItem } from '../../models/order-item';
+import { CartService } from '../../services/cart.service';
+import { OrdersService } from '../../services/orders.service';
+import { ORDER_STATUS } from '../../order.constants';
 
 @Component({
   selector: 'orders-checkout-page',
@@ -13,16 +20,19 @@ export class CheckoutPageComponent implements OnInit {
   constructor(
     private router: Router,
     private usersService: UsersService,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private cartService: CartService,
+    private ordersService: OrdersService
   ) {}
   checkoutFormGroup: FormGroup;
   isSubmitted = false;
   orderItems: OrderItem[] = [];
-  userId: string;
+  userId: '6416858655d5f4f63d931c71';
   countries = [];
 
   ngOnInit(): void {
     this._initCheckoutForm();
+    this._getCartItems();
     this._getCountries();
   }
 
@@ -52,6 +62,37 @@ export class CheckoutPageComponent implements OnInit {
     if (this.checkoutFormGroup.invalid) {
       return;
     }
+
+    const order: Order = {
+      orderItems: this.orderItems,
+      shippingAddress1: this.checkoutForm.street.value,
+      shippingAddress2: this.checkoutForm.apartment.value,
+      city: this.checkoutForm.city.value,
+      zip: this.checkoutForm.zip.value,
+      country: this.checkoutForm.country.value,
+      phone: this.checkoutForm.phone.value,
+      status: 0,
+      user: this.userId,
+      dateOrdered: `${Date.now()}`,
+    };
+
+    this.ordersService.createOrder(order).subscribe(
+      () => {
+        this.router.navigate(['/success']);
+        this.cartService.emptyCart();
+      },
+      () => {}
+    );
+  }
+
+  private _getCartItems() {
+    const cart: Cart = this.cartService.getCart();
+    this.orderItems = cart.items.map((item) => {
+      return {
+        product: item.productId,
+        quantity: item.quantity,
+      };
+    });
   }
 
   get checkoutForm() {
